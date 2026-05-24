79e4739fb682208cae7673146d4e06f683ff49ec
Brian Lalonde <brianary@gmail.com>
2026-05-23 22:24:38 -0700

    Write VHS demo.tape and add image to readme Fixes #1

---
+++ images/demo.ps1
@@ -0,0 +1,10 @@
+﻿function Send-SeqEvent([string] $Message, $Properties,
+[ValidateSet('Verbose','Debug','Information','Warning','Error','Fatal')][string] $Level,
+[uri] $Server, [string] $ApiKey, [switch] $LiteralMessage) { Start-Sleep -Seconds 2 }
+function Send-SeqScriptEvent([string]$Action,
+[Management.Automation.ErrorRecord]$ErrorRecord,
+[ValidateSet('Verbose','Debug','Information','Warning','Error','Fatal')][string] $Level = 'Error',
+[Alias('Scope')][string] $InvocationScope, [uri] $Server, [string] $ApiKey) { Start-Sleep -Seconds 2 }
+function Use-SeqServer([Parameter(Mandatory=$true)][uri] $Server, [string] $ApiKey) {}
+$result = 'Done'
+$key = "$(New-Guid)"
