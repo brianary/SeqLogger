6512b7eb9a75087f921cb06d0755cca166a20b3d
Brian Lalonde <brianary@gmail.com>
2026-05-23 17:14:05 -0700

    ✅ Fix logging test

--- test/Send-SeqEvent.Tests.ps1
+++ test/Send-SeqEvent.Tests.ps1
@@ -28,7 +28,7 @@ Describe 'Send-SeqEvent' -Tag Send-SeqEvent,Send,SeqEvent {
 			Should -Invoke -ModuleName SeqLogger -CommandName Invoke-RestMethod
 		}
 		It "should send a structured error message" {
-			Send-SeqEvent -Properties @{ Message = $Error[0].Exception.Message } -Level Error `
+			Send-SeqEvent -Properties @{ Message = 'Error while processing' } -Level Error `
 				-Server http://audit.example.net
 			Should -Invoke -ModuleName SeqLogger -CommandName Invoke-RestMethod
 		}
