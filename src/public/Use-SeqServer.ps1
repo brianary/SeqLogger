2c1b8b1df184a1b0d28e7b6c77fa27c732d88165
Brian Lalonde <brianary@gmail.com>
2026-05-27 19:55:46 -0700

    🎨 Optional param is now conditional; link to Seq

--- src/public/Use-SeqServer.ps1
+++ src/public/Use-SeqServer.ps1
@@ -18,12 +18,9 @@ Use-SeqServer.ps1 http://my-seq $apikey
 # The Seq API key to use.
 [string] $ApiKey
 )
-
 Write-Verbose "Using Seq at $Server"
-$value = @{
-    'Send-SeqEvent:Server' = $Server
-    'Send-SeqEvent:ApiKey' = $ApiKey
-}
+$value = @{ 'Send-SeqEvent:Server' = $Server }
+if($ApiKey) {$value['Send-SeqEvent:ApiKey'] = $ApiKey}
 $defaults = Get-Variable -Scope 2 -Name PSDefaultParameterValues -EA SilentlyContinue
 if($defaults) {$value.Keys |Where-Object {$defaults.Value.Contains($_)} |ForEach-Object {$defaults.Value.Remove($_)}; $defaults.Value += $value}
 else {Set-Variable -Scope 2 -Name PSDefaultParameterValues -Value $value}
